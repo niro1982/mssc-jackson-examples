5f60ddef007f36b32addbbf9c423f71958bda4c3
niro1982 <n.edinger1982@gmail.com>
2020-07-02 21:34:11 +0300

    setup property names

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -5,19 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.test.context.ActiveProfiles;
 
-/**
- * Created by jt on 2019-06-02.
- */
-@ActiveProfiles("snake")
+@ActiveProfiles("kebab")
 @JsonTest
-public class BeerDtoSnakeTest extends BaseTest {
+public class BeerDtoKebabTest extends BaseTest{
 
     @Test
-    void testSnake() throws JsonProcessingException {
+    void testKebab() throws JsonProcessingException {
         BeerDto dto = getDto();
-
         String json = objectMapper.writeValueAsString(dto);
-
         System.out.println(json);
     }
 }
